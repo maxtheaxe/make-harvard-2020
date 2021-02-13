5e6a99f04c10ff87506913b2127ad10354d2a659
Michael Kielstra <4313413+PMKielstra@users.noreply.github.com>
2021-02-13 15:49:00 -0500

    Key read-in bug fixes

--- Keyserver/server.py
+++ Keyserver/server.py
@@ -1,12 +1,13 @@
 from flask import Flask, json
 from datetime import datetime
+from functools import cmp_to_key
 
 app = Flask(__name__)
 
 keys = {}
 newest_key_id = 0
 
-keyfile = 'keys.json'
+keyfile_path = 'keys.json'
 
 date_format_string = '%m/%d/%Y'
 
@@ -16,30 +17,31 @@ def compare_date_strings(ds1, ds2):
     return date1 > date2
 
 def get_newest_key(keys):
-    return sorted(keys, cmp=lambda k1, k2: compare_date_strings(k1.date, k2.date))[0]
+    return sorted(keys, key=cmp_to_key(lambda k1, k2: compare_date_strings(k1['date'], k2['date'])))[0]
 
 @app.before_first_request
 def load_keys():
-    data = json.load(keyfile)
-    keys = data.keys
-    uncompromised = filter(lambda key: not hasattr(key, 'compromised_date'), keys)
-    priority_keys = filter(lambda key: hasattr(key, 'priority') and key.priority, uncompromised)
-    if len(priority_keys) > 0:
-        newest_key_id = get_newest_key(priority_keys).id
-    elif len(uncompromised) > 0:
-        newest_key_id = get_newest_key(uncompromised).id
-    else:
-        newest_key_id = get_newest_key(keys).id
+    with open(keyfile_path) as keyfile:
+        data = json.load(keyfile)
+        keys = data['keys']
+        uncompromised = list(filter(lambda key: not hasattr(key, 'compromised_date'), keys))
+        priority_keys = list(filter(lambda key: hasattr(key, 'priority') and key.priority, uncompromised))
+        if len(priority_keys) > 0:
+            newest_key_id = get_newest_key(priority_keys)['id']
+        elif len(uncompromised) > 0:
+            newest_key_id = get_newest_key(uncompromised)['id']
+        else:
+            newest_key_id = get_newest_key(keys)['id']
 
 @app.route('/key/<key_id>')
 def get_key(id):
     for key in keys.values():
-        if key.id == id:
-            with open(key.public_keyfile) as public_keyfile:
+        if key['id'] == id:
+            with open(key['public_keyfile']) as public_keyfile:
                 key_data = {key: public_keyfile.read()}
                 if hasattr(key, 'compromised_date'):
-                    key_data['compromised_date'] = key.compromised_date
-                if key.id != newest_key_id:
+                    key_data['compromised_date'] = key['compromised_date']
+                if key['id'] != newest_key_id:
                     key_data['superseded_by'] = newest_key_id
                 return json.jsonify(key_data)
     abort(404)
