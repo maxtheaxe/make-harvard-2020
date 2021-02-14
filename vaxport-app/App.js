b95a7b6c40ebaaedd34008b86a3053e17324759f
Shrey Arora <shreyarora2198@gmail.com>
2021-02-14 11:58:03 -0500

    App

--- vaxport-app/App.js
+++ vaxport-app/App.js
@@ -1,40 +1,68 @@
 import * as React from 'react';
-import { Button, View, Text, TextInput, StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native';
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import Constants from 'expo-constants';
+
+import * as eva from '@eva-design/eva';
+import { View, TextInput, StyleSheet } from 'react-native';
+import { ApplicationProvider, Layout, IconRegistry, Text, Button, Icon, Input } from '@ui-kitten/components';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import * as Permissions from 'expo-permissions';
 import {
   BarCodeScanner
 } from 'expo-barcode-scanner';
 
-function HomeScreen({ navigation }) {
+const HomeScreen = ({ navigation }) => {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      <Button
-        title="Doctor"
-        onPress={() => navigation.navigate('Doctor')}
-      />
-      <Button
-        title="Verifier"
-        onPress={() => navigation.navigate('Verifier')}
-      />
-    </View>
+    <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text category = 'h1'>Who are you?</Text>
+        <Button size='giant' onPress={() => navigation.navigate('Doctor')}>
+          {evaProps => <Text {...evaProps}>Doctor</Text>}
+        </Button>
+      <Button size='giant' onPress={() => navigation.navigate('Verifier')}>
+        {evaProps => <Text {...evaProps}>Verifier</Text>}
+      </Button>
+    </Layout>
   );
 }
 
-//var clinic;
-
-function DoctorScreen({navigation}) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button
-        title="Massachusetts General Hospital"
-        onPress={() => navigation.navigate('PatientInfo')}
-      />
-    </View>
-  );
+class DoctorScreen extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      clinic: ''
+    }
+  }
+  
+  render(){
+    const PinIcon = (props) => (
+      <Icon {...props} name='pin-outline'/>
+    );
+    return (
+      <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <Text category = 'h1'>Where do you work?</Text>
+        
+          <Button accessoryLeft = {PinIcon}
+          onPress={() => {
+            this.setState({
+              clinic: 'Massachusetts General Hospital'
+            });
+            this.props.navigation.navigate('PatientInfo')
+            }}>
+            {evaProps => <Text {...evaProps}>Massachusetts General Hospital</Text>}
+          </Button>
+        <Button accessoryLeft = {PinIcon}
+        onPress={() => navigation.navigate('PatientInfo')}>
+          {evaProps => <Text {...evaProps}>Foxborough: Gillette Stadium</Text>}
+        </Button>
+        <Button accessoryLeft = {PinIcon}
+        onPress={() => navigation.navigate('PatientInfo')}>
+          {evaProps => <Text {...evaProps}>Danvers: Doubletree Hotel</Text>}
+        </Button>
+      </Layout>
+    );
+  }
 }
 
 class PatientInfoScreen extends React.Component{
@@ -52,64 +80,80 @@ class PatientInfoScreen extends React.Component{
 
   render(){
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <TextInput 
-                placeholder="Enter Vaccine Type"
-                value={this.state.vaccine}
-                onChangeText={vaccine => this.setState({vaccine})}
-                returnKeyType="next"
-                underlineColorAndroid='rgba(0,0,0,0)'
-                selectionColor={'black'}
-                numberOfLines={1}/>
-        <TextInput 
-                placeholder="Enter Date"
-                value={this.state.date}
-                onChangeText={date => this.setState({date})}
-                returnKeyType="next"
-                underlineColorAndroid='rgba(0,0,0,0)'
-                selectionColor={'black'}
-                numberOfLines={1}/>
-        
-        <TextInput 
-                placeholder="Enter First Name"
-                value={this.state.firstName}
-                onChangeText={firstName => this.setState({firstName})}
-                returnKeyType="next"
-                underlineColorAndroid='rgba(0,0,0,0)'
-                selectionColor={'black'}
-                numberOfLines={1}/>
-        <TextInput 
-                placeholder="Enter Last Name"
-                value={this.state.lastName}
-                onChangeText={lastName => this.setState({lastName})}
-                returnKeyType="next"
-                underlineColorAndroid='rgba(0,0,0,0)'
-                selectionColor={'black'}
-                numberOfLines={1}/>
-        <TextInput 
-                placeholder="Enter Email"
-                value={this.state.email}
-                onChangeText={email => this.setState({email})}
-                returnKeyType="next"
-                underlineColorAndroid='rgba(0,0,0,0)'
-                selectionColor={'black'}
-                numberOfLines={1}/>
+      <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <Input
+          placeholder="Enter Vaccine Type"
+          value={this.state.vaccine}
+          onChangeText={vaccine => this.setState({vaccine})}
+          returnKeyType="next"
+          numberOfLines={1}
+        />
+        <Input
+          placeholder="Enter Date"
+          value={this.state.date}
+          onChangeText={date => this.setState({date})}
+          returnKeyType="next"
+          numberOfLines={1}
+        />
+        <Input
+          placeholder="Enter First Name"
+          value={this.state.firstName}
+          onChangeText={firstName => this.setState({firstName})}
+          returnKeyType="next"
+          numberOfLines={1}
+        />
+        <Input
+          placeholder="Enter Last Name"
+          value={this.state.lastName}
+          onChangeText={lastName => this.setState({lastName})}
+          returnKeyType="next"
+          numberOfLines={1}
+        />
+        <Input
+          placeholder="Enter Email"
+          value={this.state.email}
+          onChangeText={email => this.setState({email})}
+          returnKeyType="next"
+          numberOfLines={1}
+        />
         <Button
-          title="Submit"
           onPress={() => {
             console.log('Vaccine type:' + this.state.vaccine +
                         '\nDate:' + this.state.date + 
                         '\nFirst Name:' + this.state.firstName + 
                         '\nLastName' + this.state.lastName + 
                         '\nEmail' + this.state.email)
+            // make a post request
+            console.log('Json'+ getPatientQRCode(this.state.firstName, this.state.lastName, this.state.email))
             this.props.navigation.navigate('Home')
           }}
-        />
-      </View>
+        >
+          {evaProps => <Text {...evaProps}>Submit</Text>}
+        </Button>
+      </Layout>
     );
   }
 }
 
+ function getPatientQRCode(firstName, lastName, email) {
+  try {
+    let formdata = new FormData();
+    let data = JSON.stringify({
+      name: firstName + ' ' + lastName,
+      email: email
+    });
+    formdata.append("patient_data", data);
+    let response =  fetch('https://vaxport-mh.herokuapp.com/doctor/vaccinate', {
+      method: 'POST',
+      mode: 'no-cors',
+      body: formdata
+    });
+    let responseJson =  response.json();
+    return responseJson;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 class VerifierScreen extends React.Component  {
   state = {
@@ -174,28 +218,82 @@ class VerifierScreen extends React.Component  {
     this.setState({
       scanned: true
     });
-    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
-    this.props.navigation.navigate('Home');
+
+    getVerification(data).then((responseJson)=>{
+      this.props.navigation.navigate('ConfirmVerification',{
+        patient_name: responseJson.patient_name,
+        signatures_ok: responseJson.signatures_ok,
+        clinic_compromised: responseJson.clinic_compromised,
+        clinic_witness_compromised: responseJson.clinic_witness_compromised,
+        clinic_url: responseJson.clinic_url,
+        clinic_witness_url: responseJson.clinic_witness_url
+      })
+    });
+    
   };
 }
 
+function getVerification(data) {
+  try {
+    let formdata = new FormData();
+    
+    formdata.append("patient_data", data);
+    let response =  fetch('https://vaxport-mh.herokuapp.com/verifier/verify', {
+      method: 'POST',
+      mode: 'no-cors',
+      body: formdata
+    });
+    return response.then((response)=>{
+      return response.json();
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+const ConfirmVerificationScreen = ({ route, navigation }) => {
+  const {patient_name, signatures_ok, clinic_compromised, clinic_witness_compromised, clinic_url, clinic_witness_url} = route.params;
+  return (
+    <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text category = 'h1'>{patient_name}</Text>
+        <Text category = 'h1'>{signatures_ok}</Text>
+        <Text category = 'h1'>{clinic_compromised}</Text>
+        <Text category = 'h1'>{clinic_witness_compromised}</Text>
+        <Text category = 'h1'>{clinic_url}</Text>
+        <Text category = 'h1'>{clinic_witness_url}</Text>
+    </Layout>
+    // <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    //   <Text >Verification</Text>
+    //   <Text >{patient_name}</Text>
+    //   <Button onPress={() => navigation.navigate('Home')}>
+    //     {evaProps => <Text {...evaProps}>Home</Text>}
+    //   </Button>
+    // </Layout>
+  );
+}
 
 const Stack = createStackNavigator();
 
 function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen
-        name="Home"
-        component={HomeScreen}
-        options={{ title: 'Overview' }}
-        />
-        <Stack.Screen name="Doctor" component={DoctorScreen} />
-        <Stack.Screen name="Verifier" component={VerifierScreen} />
-        <Stack.Screen name="PatientInfo" component={PatientInfoScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <>
+    <IconRegistry icons={EvaIconsPack}/>
+      <ApplicationProvider {...eva} theme={eva.dark}>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Home">
+            <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{ title: 'Overview' }}
+            />
+            <Stack.Screen name="Doctor" component={DoctorScreen} />
+            <Stack.Screen name="Verifier" component={VerifierScreen} />
+            <Stack.Screen name="PatientInfo" component={PatientInfoScreen} />
+            <Stack.Screen name="ConfirmVerification" component={ConfirmVerificationScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </ApplicationProvider>
+      </>
   );
 }
 
